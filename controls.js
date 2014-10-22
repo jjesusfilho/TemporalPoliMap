45abc812abc7e248cb3d90453422e3a9ad0124ee
Shubhanshu Mishra <shubhanshumishra@gmail.com>
2014-10-22 18:26:08 -0500

    Removed redundant files. Updated some code and updated libraries in bower.

---
+++ controls.js
@@ -0,0 +1,43 @@
+var curr_country = "USA";
+var curr_year = 2007;
+var curr_obj = null;
+var curr_obj1 = null;
+$(function () {
+	// body...
+	change_country(curr_country, curr_year);
+	function valueOutput(element) {
+        var value = element.value,
+            output = element.parentNode.getElementsByTagName('output')[0];
+        output.innerHTML = value;
+    }
+	$(".chosen-select").chosen().change(function(e){
+		console.log(e.target.value);
+		$('#container').html("");
+		curr_country = e.target.value;
+		change_country(curr_country, curr_year);
+	});
+	$('[data-rangeslider]').rangeslider({
+
+        // Deactivate the feature detection
+        polyfill: false,
+
+        // Callback function
+        onInit: function() {
+        	valueOutput(this.$element[0]);
+        	window.setTimeout(function() {}, 60000);
+        },
+
+        // Callback function
+        onSlideEnd: function(position, value) {
+            console.log('onSlideEnd');
+            console.log('position: ' + position, 'value: ' + value);
+            $('#container').html("");
+    		curr_year = value;
+    		change_country(curr_country, curr_year);
+    		curr_obj = $(this);
+    		curr_obj1 = this;
+    		console.log("Current Object: ", $(this));
+    		valueOutput(this.$element[0]);
+        }
+    });
+});
